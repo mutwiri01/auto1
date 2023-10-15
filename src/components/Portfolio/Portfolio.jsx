716c24f5b07ae1b89ec04e4b4a96a8c43b8e3a2c
mutwiri01 <mutwirikithinji01@gmail.com>
2023-10-15 23:36:33 +0300

    56

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,19 +1,21 @@
 import React, { useContext } from "react";
 import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react"
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import n1 from "../../img/n1.jpg";
 import g1 from "../../img/g1.jpg";
 import s1 from "../../img/s1.jpg";
 import p1 from "../../img/p1.jpg";
 import { themeContext } from "../../Context";
+
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{color: darkMode?'white': ''}}>AUTOPEC</span>
+      <span style={{ color: darkMode ? "white" : "" }}>AUTOPEC</span>
       <span>SERVICE GALLERY</span>
 
       {/* slider */}
