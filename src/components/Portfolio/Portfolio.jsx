a381acc45862ab3b152c546016bdce9406c51851
mutwiri01 <mutwirikithinji01@gmail.com>
2023-10-15 23:12:26 +0300

    67

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,21 +1,19 @@
 import React, { useContext } from "react";
 import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
 import n1 from "../../img/n1.jpg";
 import g1 from "../../img/g1.jpg";
 import s1 from "../../img/s1.jpg";
 import p1 from "../../img/p1.jpg";
 import { themeContext } from "../../Context";
-
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
-
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{ color: darkMode ? "white" : "" }}>AUTOPEC</span>
+      <span style={{color: darkMode?'white': ''}}>AUTOPEC</span>
       <span>SERVICE GALLERY</span>
 
       {/* slider */}
@@ -28,28 +26,18 @@ const Portfolio = () => {
         <SwiperSlide>
           <img src={n1} alt="" />
           <h1>Engine Overhaul:</h1>
-          <h2>
-            Our certified technicians specialize in engine overhauls, breathing
-            new life into your vehicle's heart.
-          </h2>
         </SwiperSlide>
         <SwiperSlide>
           <img src={g1} alt="" />
           <h1>Gearbox Repairs:</h1>
-          <h2>
-            Our gearbox repair experts are equipped to diagnose and fix any
-            transmission issues, whether it's a manual or automatic gearbox
-          </h2>
         </SwiperSlide>
         <SwiperSlide>
           <img src={p1} alt="" />
           <h1>Paint Spraying:</h1>
-          <h2>Restore your vehicle's appearance with our top-notch paint spraying services.</h2>
         </SwiperSlide>
         <SwiperSlide>
           <img src={s1} alt="" />
           <h1>Brake and Suspension Services</h1>
-          <h2>From brake pad replacements to suspension system upgrades, we've got you covered</h2>
         </SwiperSlide>
       </Swiper>
     </div>
